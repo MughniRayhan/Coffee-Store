fcb1779b69a603d78549baa4b7679016aea532df
MughniRayhan <tishtisha13@gmail.com>
2025-05-17 23:53:49 +0600

    setup mongodb

--- coffee-store-server/index.js
+++ coffee-store-server/index.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+// tYAfIX9sEXUyb7SU
+// coffee_monster
+// const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.iuxl4dg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iuxl4dg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Coffee server is getting hotter!');
