d33e805bad30e7a54f5bfdc990d964a3f6c708df
MughniRayhan <tishtisha13@gmail.com>
2025-05-19 02:48:05 +0600

    delete coffee

--- coffee-store-client/src/Components/CoffeCard.jsx
+++ coffee-store-client/src/Components/CoffeCard.jsx
@@ -2,9 +2,44 @@ import React from 'react'
 import { IoEye } from "react-icons/io5";
 import { MdEdit } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
+import Swal from 'sweetalert2';
 
 function CoffeCard({coffee}) {
-  const {name, chef, supplier, taste, photo, price} = coffee;
+  const {_id,name, chef, photo, price} = coffee;
+
+  const handleDelete = (id) => {
+
+   Swal.fire({
+  title: "Are you sure?",
+  text: "You won't be able to revert this!",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Yes, delete it!"
+}).then((result) => {
+  if (result.isConfirmed) {
+    fetch(`http://localhost:3000/coffees/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      if(data.deletedCount) {
+       Swal.fire({
+      title: "Deleted!",
+      text: "Your Coffee has been deleted.",
+      icon: "success"
+    });
+      }
+    })
+   
+  }
+});
+
+  }
   return (
     
             <div className='flex flex-col md:flex-row justify-between items-center bg-[#F5F4F1] p-[30px] rounded-xl gap-5'>
@@ -18,7 +53,7 @@ function CoffeCard({coffee}) {
             <div className='flex md:flex-col gap-4'>
                 <div className='bg-[#D2B48C] p-3 text-white rounded'><IoEye /></div>
                <div  className='bg-[#3C393B] p-3 text-white rounded'><MdEdit/></div>
-               <div  className='bg-[#EA4744] p-3 text-white rounded'><MdDelete/></div>
+               <div  className='bg-[#EA4744] p-3 text-white rounded' onClick={()=>handleDelete(_id)}><MdDelete/></div>
             </div>
            
     </div>
--- coffee-store-server/index.js
+++ coffee-store-server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -33,10 +33,25 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.get('/coffees/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post('/coffees', async (req, res) => {
       const newCoffee = req.body;
       console.log(newCoffee);
       const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    });
+
+    app.delete('/coffees/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await coffeeCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
